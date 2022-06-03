32a9739753861d218d04d033fed1813cb2e26697
Alippp1 <aliftafn03@upi.edu>
2022-06-03 19:36:43 +0700

    d

--- uaspbo/src/login/login.java
+++ uaspbo/src/login/login.java
@@ -130,7 +130,7 @@ public class login extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+        // TODO add your handling code here: gsdsgsf
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
